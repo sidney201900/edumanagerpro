3ee328f1d4a60e999e146dad793346ed1d3ec299
sidney201900 <51304373+sidney201900@users.noreply.github.com>
2026-03-05 22:02:36 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,17 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
-  
-  return {
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    // Injeção direta no objeto global do Vite
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify('https://ekbuvcjsfcczviqqlfit.supabase.co'),
+    'import.meta.env.VITE_SUPABASE_KEY': JSON.stringify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrYnV2Y2pzZmNjenZpcXFsZml0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5OTU0MzIsImV4cCI6MjA4NjU3MTQzMn0.oIzBeGF-PjaviZejYb1TeOOEzMm-Jjth1XzvJrjD6us')
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
-    plugins: [react()],
-    define: {
-      'process.env.VITE_SUPABASE_URL': JSON.stringify('https://ekbuvcjsfcczviqqlfit.supabase.co'),
-      'process.env.VITE_SUPABASE_KEY': JSON.stringify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrYnV2Y2pzZmNjenZpcXFsZml0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5OTU0MzIsImV4cCI6MjA1NjgwNzg5N30.oIzBeGF-PjaviZejYb1TeOOEzMm-Jjth1XzvJrjD6us'),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-  };
+  },
 });
